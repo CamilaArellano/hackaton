76c5a0b42bd62260434feb704b9f56d5b2e3d668
Camila Arellano <eiracamila1@gmail.com>
2024-03-15 16:33:06 -0600

    Maquetar read_xlsx

--- read_xlsx.py
+++ read_xlsx.py
@@ -1,25 +1,25 @@
 import openpyxl
 
-def data(file_path):
-    excel_dataFrame = openpyxl.load_workbook(file_path)
-    name_dataFrame = excel_dataFrame.active
+def fetch_data(file_path):
+    excel_workbook = openpyxl.load_workbook(file_path)
+    active_sheet = excel_workbook.active
 
     data = []
-    for row in name_dataFrame.iter_rows():
-        empty_row = True  # La fila está vacía
+    for row in active_sheet.iter_rows():
+        empty_row = True  # Assume the row is empty
         _row = []
-        for cell in row[:30]:  # interaciones hasta el 30 en cuanto a columnas
-            if cell.value is not None:  # Se checa si está vacía  # Se agrega si es que no está vacía
+        for cell in row[:30]:  # Iterate through columns up to 30
+            if cell.value is not None:  # Check if the cell is not empty
                 empty_row = False
-            _row.append(cell.value) # Se cambia la condición
-        if not empty_row:  # Si la fila al final de las condiciones no está vacía, se guarda
-            data.append(_row)  # Se agrega a la lista
-    data.pop(0)
-    data.pop()
+            _row.append(cell.value)  # Add the cell value to the row
+        if not empty_row:  # If the row is not empty after checking
+            data.append(_row)  # Add it to the list
+    data.pop(0)  # Remove the first row (assuming it's a header)
+    data.pop()   # Remove the last row (assuming it's empty)
     return data
 
 # Print the data
 file_path = 'files/Hackathon-Information.xlsx'
-data_generados=data(file_path)
-for row in data_generados:
+generated_data = fetch_data(file_path)
+for row in generated_data:
     print(row)
